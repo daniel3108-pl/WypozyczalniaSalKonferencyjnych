81b37ebb69481d5072e967c4e673c3cfd7c05af6
Daniel Swietlik <56155234+daniel3108-pl@users.noreply.github.com>
2022-01-26 22:41:11 +0100

    Update WypozyczalniaModel.py

--- source/WarstwaPrezentacji/WypozyczalniaModel.py
+++ source/WarstwaPrezentacji/WypozyczalniaModel.py
@@ -1,14 +1,4 @@
-"""
-Kod Zródłowy do projektu IO Wypożyczalnia Sal Konferencyjnych
-Informatyka Katowice Grupa 2 Sekcja 5
 
-Bartosz Padkowski
-Daniel Świetlik
-Kamil Tlałka
-Jan Słowik
-Piotr Jankowski
-Cezary Szumerowski
-"""
 
 from flask import *
 import config as Konfiguracje
